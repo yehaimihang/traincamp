36e5bc604058e97e68f0ce9f1ff6891143485896
lizhonghang <422607222@qq.com>
2020-05-18 20:27:47 +0800

    all

---
+++ src/main/java/com/example/traincamp/TraincampApplication.java
@@ -0,0 +1,13 @@
+package com.example.traincamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraincampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraincampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/traincamp/TraincampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.traincamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraincampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
